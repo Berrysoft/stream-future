a55cb9c44238b093bd8c241c2530c5bf7bc090bc
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-08-26 08:55:23 +0800

    Exact code here.

---
+++ stream-future/tests/lifetime.rs
@@ -0,0 +1,15 @@
+#![feature(generators)]
+
+use stream_future::*;
+
+#[tokio::test]
+async fn lifetime() {
+    #[stream(lifetime = "'a")]
+    async fn foo<'a>(s: &'a str) {
+        yield;
+        println!("{}", s);
+        yield;
+    }
+
+    foo("Hello world!").await;
+}
